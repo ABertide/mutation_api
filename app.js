820f5889957dded0f5ec5d7ca1dfa2405897ad74
Bertide Allan (ext) FRSS <allan.bertide@syngenta.com>
2022-06-03 17:05:50 +0200

    add route to mutation

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
 const app = express();
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    next();
+});
 app.use('/api/mutation', (req, res, next) => {
     let json = require('./data/data.json');
-    res.status(200).json(json);
+    res.json(json);
 });
 
 app.use((req, res, next) => {
